c1622fabea02b57e539ff656dce8aaf4b1af8656
haxul <s.s.starodubov@yandex.ru>
2020-09-16 14:55:01 +0400

    concurrency fix

--- concurrency/main.go
+++ concurrency/main.go
@@ -2,31 +2,36 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
 var (
 	channel = make(chan int, 2)
+	num     = 0
+	m       sync.Mutex
+	wg      sync.WaitGroup
 )
 
 func main() {
 	go push()
-
-	for true {
-		num, ok := <-channel
-		if ok {
-			time.Sleep(5 * time.Second)
-			fmt.Printf("got %d from channel\n", num)
-		} else {
-			fmt.Printf("end")
-			break
-		}
+	for num = range channel {
+		time.Sleep(3 * time.Second)
+		fmt.Printf("got %d from channel\n", num)
 	}
 
+	//wg.Add(1)
+	//go HelloWorld()
+	//wg.Wait()
+}
+
+func HelloWorld() {
+	println("hello world")
+	wg.Done()
 }
 
 func push() {
-	for i := 0; i < 6; i++ {
+	for i := 0; i < 3; i++ {
 		channel <- i
 		fmt.Printf("push %d in channel\n", i)
 		time.Sleep(1 * time.Second)
